bc619259a54a6687d12ef63a0dfe045a48775274
João Gonçalves <jandretgoncalves@gmail.com>
2022-05-24 22:27:16 +0100

    Remove vitest setup

--- vitest-setup.ts
+++
@@ -1,9 +0,0 @@
-// Mock IntersectionObserver
-const IntersectionObserverMock = vi.fn(() => ({
-  disconnect: vi.fn(),
-  observe: vi.fn(),
-  takeRecords: vi.fn(),
-  unobserve: vi.fn(),
-}))
-
-vi.stubGlobal('IntersectionObserver', IntersectionObserverMock)
